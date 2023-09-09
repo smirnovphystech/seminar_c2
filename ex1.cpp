f3310d8ac37c7ad0ae1681b60218a0452ffc4a15
smirnovphystech <smirnov.phystech@gmail.com>
2023-09-09 10:47:30 +0300

    ex1

--- ex1.cpp
+++ ex1.cpp
@@ -8,24 +8,22 @@ int fib(int n){
     fn2 = 1;
     fn = fn1 + fn2;
     for(int i = 0; i<n-3; i++){
-        fn1 = fn2;
-        fn2 = fn;
+        fn2 = fn1;
+        fn1 = fn;
         fn = fn1 + fn2;
     }
     return fn;
 }
 
 int main(){
-    int n;
-    cin >> n;
-
-    if (n == 1 || n==2) {
-        cout << 1;
+    for(int n = 1; n<15; n++){
+        if (n == 1 || n==2) {
+            cout << n << ":" << 1 << endl;
+        }
+        else{
+            int res = fib(n);
+            cout << n << ":" << res << endl;
+        }
     }
-    else{
-        int res = fib(n);
-        cout << res;
-    }
-
     return 0;
 }
